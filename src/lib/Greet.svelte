195edcc205998ad00cce74dd02a1b709979f946e
Madhav Ghei <madhav.ghei@gmail.com>
2023-08-05 16:24:14 -0500

    Add tauri files

---
+++ src/lib/Greet.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { invoke } from '@tauri-apps/api/tauri'
+
+    let name = ''
+    let greetMsg = ''
+
+    async function greet() {
+        greetMsg = await invoke('greet', { name })
+    }
+</script>
+
+<div>
+    <input id="greet-input" bind:value={name} placeholder="Enter a name..."/>
+    <button on:click={greet}>Greet</button>
+    <p>{greetMsg}</p>
+</div>
